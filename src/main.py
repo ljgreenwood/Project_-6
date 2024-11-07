90005e266a482edfd67076f2dc7036dff9aa19ed
Lucas Greenwood <168799860+ljgreenwood@users.noreply.github.com>
2024-11-07 12:14:22 -0500

    up-to-date in class main tictactoe

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # HEY! -- Final Project
-import pygame, sys
+import pygame, sys, tictactoe
 from constants import *
 
 pygame.init()
@@ -7,6 +7,11 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic Tac Toe")
 
 screen.fill(BG_COLOR)
+chip_font = pygame.font.Font(None, CHIP_FONT)
+
+board = tictactoe.initialize_board()
+board[1][1] = "x"
+board[1][0] = "o"
 
 
 def draw_grid():
@@ -30,13 +35,47 @@ def draw_grid():
         )
 
 
-draw_grid()
+def draw_chips():
+    chip_x_surf = chip_font.render("x", 0, CROSS_COLOR)
+    chip_o_surf = chip_font.render("o", 0, CIRCLE_COLOR)
 
-while True:
-    # event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == "x":
+                chip_x_rect = chip_x_surf.get_rect(
+                    center=(
+                        SQUARE_SIZE * col + SQUARE_SIZE / 2,
+                        SQUARE_SIZE * row + SQUARE_SIZE / 2,
+                    )
+                )
+                screen.blit(chip_x_surf, chip_x_rect)
+            elif board[row][col] == "o":
+                chip_o_rect = chip_o_surf.get_rect(
+                    center=(
+                        SQUARE_SIZE * col + SQUARE_SIZE / 2,
+                        SQUARE_SIZE * row + SQUARE_SIZE / 2,
+                    )
+                )
+                screen.blit(chip_o_surf, chip_o_rect)
 
-    pygame.display.update()
+
+def main():
+    draw_grid()
+    draw_chips()
+    while True:
+        # event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos  # tuple of coordinates
+                print(x, y)
+                row, col = (x // 200, y // 200)
+                print(row, col)
+
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
